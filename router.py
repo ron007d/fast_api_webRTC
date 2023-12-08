56a6a1dd69bf40c73ccceecc3936938962f80cce
ron007d <rohan42010@gmail.com>
2023-12-08 11:09:13 +0530

    Added call_page redirection + answer_page redirection

--- router.py
+++ router.py
@@ -8,14 +8,29 @@ import uvicorn
 app = FastAPI()
 templates = Jinja2Templates('templates')
 
-testing_dict = {'Call' : {'url':'google.com'},
-                'answer' : {'url': 'duckduckgo.com'}}
+testing_dict = {'Call' : {'url':'call_page'},
+                'answer' : {'url': 'answer_page'}}
 
 
 @app.get('/')
 def home(request : Request):
-    print(request.url)
+    # print(request.url)
     return templates.TemplateResponse('home.html',{"request" : request, 'data': testing_dict })
+
+@app.get('/call_page')
+def call_page(request: Request):
+    return {'Message' : f'you have came to URL page {request.url}'}
+
+@app.post('offer_data')
+def saving_offers(user_name, offer):
+    print(user_name)
+    print(offer)
+    return {'success' : True}
+
+
+@app.get('/answer_page')
+def answer_page(request: Request):
+    return {'Message' : f'you have came to URL page {request.url}'}
 
 
 
